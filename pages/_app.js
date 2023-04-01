6084ec9666b1f83c0097274dcb46cccdcb58e419
Yannick MATH <yannick_math@yahoo.fr>
2023-04-01 20:42:02 +0200

    maj responsive portrait paysage test

--- pages/_app.js
+++ pages/_app.js
@@ -11,11 +11,43 @@ export default function MyApp({ Component, pageProps }) {
 
   const [isSmallScreen, setIsSmallScreen] = useState(false);
 
-  const updateScreenSize = () => {
-    const mediaQuery = window.matchMedia("(max-width: 768px)");
-    setIsSmallScreen(mediaQuery.matches);
-  };
+  // const updateScreenSize = () => {
+  //   const mediaQuery = window.matchMedia("(max-width: 768px)");
+  //   setIsSmallScreen(mediaQuery.matches);
+  // };
 
+
+  const updateScreenSize = () => {
+    const isPortrait = window.matchMedia("(orientation: portrait)").matches;
+    const isTablet = window.matchMedia("(min-width: 768px)").matches;
+  
+    // Verrouiller le mode portrait sur les smartphones
+    if (isSmallScreen && !isPortrait) {
+      document.documentElement.style.transform = "rotate(0deg)";
+      document.documentElement.style.width = "100vw";
+      document.documentElement.style.height = "100vh";
+      document.documentElement.style.overflow = "hidden";
+    } else {
+      document.documentElement.style.transform = "none";
+      document.documentElement.style.width = "auto";
+      document.documentElement.style.height = "auto";
+      document.documentElement.style.overflow = "visible";
+    }
+  
+    // Verrouiller le mode paysage sur les tablettes
+    if (!isSmallScreen && isTablet && isPortrait) {
+      document.documentElement.style.transform = "rotate(90deg)";
+      document.documentElement.style.width = "100vh";
+      document.documentElement.style.height = "100vw";
+      document.documentElement.style.overflow = "hidden";
+    } else {
+      document.documentElement.style.transform = "none";
+      document.documentElement.style.width = "auto";
+      document.documentElement.style.height = "auto";
+      document.documentElement.style.overflow = "visible";
+    }
+  };
+  
   useEffect(() => {
     updateScreenSize(); // Call the function once to set the initial value
     window.addEventListener("resize", updateScreenSize); // Listen for window resize events
@@ -26,7 +58,6 @@ export default function MyApp({ Component, pageProps }) {
     };
   }, []);
 
-  
   return (
     <Layout isSmallScreen={isSmallScreen} >
       <Component {...pageProps} key={router.route} isSmallScreen={isSmallScreen}  />
