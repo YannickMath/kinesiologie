2dd2c94168b18957d9ebd483305d7364a08a2e37
Yannick MATH <yannick_math@yahoo.fr>
2023-04-01 23:14:51 +0200

    maj

--- pages/_app.js
+++ pages/_app.js
@@ -11,44 +11,11 @@ export default function MyApp({ Component, pageProps }) {
 
   const [isSmallScreen, setIsSmallScreen] = useState(false);
 
-  // const updateScreenSize = () => {
-  //   const mediaQuery = window.matchMedia("(max-width: 768px)");
-  //   setIsSmallScreen(mediaQuery.matches);
-  // };
-
   const updateScreenSize = () => {
-    const isPortrait = window.matchMedia("(orientation: portrait)").matches;
-    const isTablet = window.matchMedia("(min-width: 768px)").matches;
-
-    // Verrouiller le mode portrait sur les smartphones
-    // if (isSmallScreen && !isPortrait) {
-    //   document.documentElement.style.transform = "rotate(0deg)";
-    //   document.documentElement.style.width = "100vw";
-    //   document.documentElement.style.height = "100vh";
-    //   document.documentElement.style.overflow = "hidden";
-    // } else {
-    //   document.documentElement.style.transform = "none";
-    //   document.documentElement.style.width = "auto";
-    //   document.documentElement.style.height = "auto";
-    //   document.documentElement.style.overflow = "visible";
-    // }
-
-    // Verrouiller le mode paysage sur les tablettes
-    if (!isSmallScreen && isTablet && isPortrait) {
-      document.documentElement.style.transform = "rotate(90deg)";
-      document.documentElement.style.width = "100vh";
-      document.documentElement.style.height = "100vw";
-      document.documentElement.style.overflow = "hidden";
-    } else {
-      document.documentElement.style.transform = "none";
-      document.documentElement.style.width = "auto";
-      document.documentElement.style.height = "auto";
-      document.documentElement.style.overflow = "visible";
-    }
+    const mediaQuery = window.matchMedia("(max-width: 768px)");
+    setIsSmallScreen(mediaQuery.matches);
   };
 
-  
-  
   useEffect(() => {
     updateScreenSize(); // Call the function once to set the initial value
     window.addEventListener("resize", updateScreenSize); // Listen for window resize events
