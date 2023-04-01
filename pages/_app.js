73b97bd2b966a1b9b905e82d500ae54fbf58c9e3
Yannick MATH <yannick_math@yahoo.fr>
2023-04-01 20:51:21 +0200

    maj

--- pages/_app.js
+++ pages/_app.js
@@ -35,18 +35,27 @@ export default function MyApp({ Component, pageProps }) {
     }
   
     // Verrouiller le mode paysage sur les tablettes
-    // if (!isSmallScreen && isTablet && isPortrait) {
-    //   document.documentElement.style.transform = "rotate(90deg)";
-    //   document.documentElement.style.width = "100vh";
-    //   document.documentElement.style.height = "100vw";
-    //   document.documentElement.style.overflow = "hidden";
-    // } else {
-    //   document.documentElement.style.transform = "none";
-    //   document.documentElement.style.width = "auto";
-    //   document.documentElement.style.height = "auto";
-    //   document.documentElement.style.overflow = "visible";
-    // }
+    if (!isSmallScreen && isTablet && isPortrait) {
+      document.documentElement.style.transform = "rotate(90deg)";
+      document.documentElement.style.width = "100vh";
+      document.documentElement.style.height = "100vw";
+      document.documentElement.style.overflow = "hidden";
+  
+      // Réinitialiser le menu burger
+      $(document).ready(function() {
+        $('.menu-toggle').removeClass('active');
+        $('.nav').removeClass('active');
+      });
+    } else {
+      document.documentElement.style.transform = "none";
+      document.documentElement.style.width = "auto";
+      document.documentElement.style.height = "auto";
+      document.documentElement.style.overflow = "visible";
+    }
   };
+  
+  
+
   
   useEffect(() => {
     updateScreenSize(); // Call the function once to set the initial value
