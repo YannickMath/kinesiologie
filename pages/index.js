18b8998fb3ed011efbf2ffbc0bd68377c4644d9e
Yannick MATH <yannick_math@yahoo.fr>
2023-04-08 09:50:35 +0200

    maj stats site

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,27 @@
 
 import Accueil from "../components/Accueil"
 
+import Script from "next/script"; 
 
 
 export default function Home() {
   return (
     <div style={{ height: "100vh" }}>
-      
+       <Script
+        src="
+https://www.googletagmanager.com/gtag/js?id=NEXT_PUBLIC_GA_ID
+"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'NEXT_PUBLIC_GA_ID');
+        `}
+      </Script> 
       <Accueil />
     </div>
   );
